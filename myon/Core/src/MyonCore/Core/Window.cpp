18d59a31c753e3b15697859b9ab34eaffb3f3a90
hangocdinhnew <dinhndrwnew@gmail.com>
2025-06-25 18:43:59 +0700

    Nix.

--- myon/Core/src/MyonCore/Core/Window.cpp
+++ myon/Core/src/MyonCore/Core/Window.cpp
@@ -9,7 +9,14 @@ Window::Window(int width, int height, const std::string &title) {
 
 Window::~Window() { cleanupWindow(); }
 
+static void customSDLLogger(void*, int category, SDL_LogPriority priority, const char* message) {
+  MYON_CORE_DEBUG("SDL Debug Message (category: {}, priority: {}) - {}", category, static_cast<int>(priority), message);
+}
+
 void Window::initWindow(int width, int height, const std::string &title) {
+  SDL_LogOutputFunction(customSDLLogger);
+  SDL_SetLogPriorities(SDL_LOG_PRIORITY_VERBOSE);
+
   MYON_CORE_ASSERT(!SDL_Init(SDL_INIT_VIDEO), "SDL - SDL_Init failed: {}",
                    SDL_GetError());
 
