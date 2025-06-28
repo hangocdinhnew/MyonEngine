08c534ac5f948695ab9776a50e4b913350274dec
hangocdinhnew <dinhndrwnew@gmai.com>
2025-06-28 07:59:08 +0700

    Fix for Windows.

--- xmake.lua
+++ xmake.lua
@@ -5,18 +5,14 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("cxx20")
 
-if is_plat("windows") and not is_tool("msvc") then
-  add_ldflags("-static", { force = true })
-end
-
 if is_mode("debug") or is_mode("asan") then
   set_policy("build.sanitizer.address", true)
   add_defines("MYON_DEBUG")
 end
 
 add_requires("wgpu-native")
-add_requires("libsdl3 master")
-add_requires("glm master")
-add_requires("spdlog master")
+add_requires("libsdl3")
+add_requires("glm")
+add_requires("spdlog")
 
 includes("myon/Core", "myon/Sandbox", "thirdparty/sdl3webgpu.lua")
