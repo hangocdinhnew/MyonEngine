a4d69e8457784a8a48912c84195611e1265f8ce3
hangocdinhnew <dinhndrwnew@gmail.com>
2025-06-29 21:15:41 +0700

    Fix.

--- myon/Core/include/MyonCore/Graphics/WebGPU/WebGPUUtils.hpp
+++ myon/Core/include/MyonCore/Graphics/WebGPU/WebGPUUtils.hpp
@@ -11,7 +11,8 @@ namespace WebGPU {
 
 // Strings
 std::string_view toStdStringView(WGPUStringView p_wgpuStringView);
-WGPUStringView toWGPUStringView(std::string p_stdString);
+WGPUStringView toWGPUStringView(const std::string &p_stdString);
+WGPUStringView toWGPUStringView(const char *p_stringliteral);
 
 } // namespace WebGPU
 } // namespace Graphics
--- myon/Core/src/MyonCore/Graphics/WebGPU/WebGPUUtils.cpp
+++ myon/Core/src/MyonCore/Graphics/WebGPU/WebGPUUtils.cpp
@@ -1,4 +1,6 @@
 // clang-format off
+#include <cstring>
+
 #include "MyonCore/Graphics/WebGPU/WebGPUUtils.hpp"
 // clang-format on
 
@@ -7,13 +9,16 @@ namespace Graphics {
 namespace WebGPU {
 
 std::string_view toStdStringView(WGPUStringView p_wgpuStringView) {
-  std::string_view stringview(p_wgpuStringView.data, p_wgpuStringView.length);
-  return stringview;
+  return std::string_view(p_wgpuStringView.data, p_wgpuStringView.length);
 }
 
-WGPUStringView toWGPUStringView(std::string p_stdString) {
+WGPUStringView toWGPUStringView(const std::string &p_stdString) {
   WGPUStringView wgpustringview = {p_stdString.data(), p_stdString.size()};
   return wgpustringview;
+}
+
+WGPUStringView toWGPUStringView(const char *p_stringliteral) {
+  return {p_stringliteral, strlen(p_stringliteral)};
 }
 
 } // namespace WebGPU
