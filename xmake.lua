e57d8fa0de4d7db5460402d93750f3bf84b8d2be
hangocdinhnew <dinhndrwnew@gmail.com>
2025-07-01 22:27:58 +0700

    Revert "Transition back to CMake."
    This reverts commit ef1021d3e0eac86545d18fd981f7dd0008a64eb9.

---
+++ xmake.lua
@@ -0,0 +1,33 @@
+set_project("Myon")
+set_version("0.1")
+
+add_rules("mode.debug", "mode.release")
+
+on_config(function(target)
+  if (target:has_tool("cxx", "cl")) then
+    target:add("cxxflags", "/utf-8")
+  end
+end)
+
+set_languages("cxx20")
+
+if is_mode("debug") or is_mode("asan") then
+  set_policy("build.sanitizer.address", true)
+  add_defines("MYON_DEBUG")
+end
+
+add_requires("wgpu-native")
+add_requires("libsdl3")
+add_requires("glm")
+add_requires("spdlog")
+
+option("enable_logtofile")
+  set_default(false)
+  set_showmenu(true)
+  set_description("Enable spdlog to log to a file (by default Myon.log)")
+  add_defines("LOGTOFILE")
+option_end()
+
+add_options("enable_logtofile")
+
+includes("myon/Core", "myon/Sandbox", "thirdparty/sdl3webgpu.lua")
