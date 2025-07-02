89b37fe4916e184bdeb9e45fe5d94e72aa506710
hangocdinhnew <dinhndrwnew@gmail.com>
2025-07-02 12:31:43 +0700

    Remove .app support and add RPATH.

--- myon/Sandbox/xmake.lua
+++ myon/Sandbox/xmake.lua
@@ -1,9 +1,4 @@
 target("MyonSandbox")
-    if is_plat("macosx") then
-      add_rules("xcode.application")
-      add_files("resources/Info.plist")
-    end
-
     set_kind("binary")
     set_languages("cxx20")
 
@@ -11,3 +6,10 @@ target("MyonSandbox")
 
     add_deps("MyonCore")
 
+    if is_plat("linux") then
+      add_rpathdirs("$ORIGIN/../lib", {installonly = true})
+    end
+
+    if is_plat("macosx") then
+      add_rpathdirs("@executable_path/../lib", {installonly = true})
+    end
