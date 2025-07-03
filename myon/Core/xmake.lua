d8381118ad9bf3af251d2ebe0da3d96272c5f3a7
hangocdinhnew <dinhndrwnew@gmail.com>
2025-07-03 13:48:44 +0700

    Use recursive.

--- myon/Core/xmake.lua
+++ myon/Core/xmake.lua
@@ -12,18 +12,5 @@ target("MyonCore")
     add_packages("spdlog", {public = true})
 
     add_files(
-        "src/MyonCore/Core/Log.cpp",
-        "src/MyonCore/Layers/LayerStack.cpp",
-        "src/MyonCore/Core/Engine.cpp",
-        "src/MyonCore/Core/Window.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUUtils.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUInstance.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUSurface.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUAdapter.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUDevice.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUBuffer.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUCommandQueue.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUComputePipeline.cpp",
-        "src/MyonCore/Graphics/WebGPU/WebGPUAPI.cpp",
-        "src/MyonCore/Graphics/GraphicsAPI.cpp"
+        "src/**/*.cpp"
     )
