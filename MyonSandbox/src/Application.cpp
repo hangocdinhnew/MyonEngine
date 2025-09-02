4ece91b3b4cb2706e6099619520a06ee452f3fc5
hangocdinhnew <dinhndrwnew@gmail.com>
2025-09-02 19:23:20 +0700

    SDL3

--- MyonSandbox/src/Application.cpp
+++ MyonSandbox/src/Application.cpp
@@ -9,9 +9,25 @@ int main()
     };
 
     MyonR::MyonRuntime runtime{&runtimeConfig};
+    auto gfx = runtime.getGraphics();
+    gfx->setSwapchainParameters(SDL_GPU_SWAPCHAINCOMPOSITION_SDR, SDL_GPU_PRESENTMODE_VSYNC);
 
     while (!runtime.shouldClose)
     {
         runtime.PollEvents();
+        gfx->acquireCmdBuffer();
+        gfx->acquireTexture();
+
+        auto colorTargetInfo = gfx->createColorTargetInfo();
+        colorTargetInfo.clear_color = { 255.0f, 0.0f, 0.0f, 255.0f };
+        colorTargetInfo.load_op = SDL_GPU_LOADOP_CLEAR;
+        colorTargetInfo.store_op = SDL_GPU_STOREOP_STORE;
+        colorTargetInfo.mip_level = 0;
+        colorTargetInfo.layer_or_depth_plane = 0;
+        colorTargetInfo.cycle = false;
+
+        auto renderPass = gfx->beginRenderPass(&colorTargetInfo, 1);
+        gfx->endRenderPass(renderPass);
+        gfx->submit();
     }
 }
